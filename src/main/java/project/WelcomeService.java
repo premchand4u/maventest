01214a959da3002f8f7393c66c70cbeac7a27d5f
prem <premchand4u@gmail.com>
2022-01-10 19:54:07 +0000

    maven project

---
+++ src/main/java/project/WelcomeService.java
@@ -0,0 +1,17 @@
+package project;
+
+import org.eiichiro.gig.Service;
+import org.eiichiro.jaguar.inject.Inject;
+import org.eiichiro.jaguar.inject.Name;
+
+@Name("project.WelcomeService")
+@Service
+public class WelcomeService {
+
+	@Inject private WelcomeRepository repository;
+	
+	public Welcome get() {
+		return repository.get();
+	}
+	
+}
